6c9361f2738e6340b472afbf9bc7fb0f38997039
pipranja <pipranja@cisco.com>
2021-10-22 13:49:34 +0530

    js bugs fixed

--- timer.js
+++ timer.js
@@ -4,8 +4,12 @@ var reset = document.getElementById("reset");
 const hour = document.getElementById("thour");
 const min = document.getElementById("tmin");
 const sec = document.getElementById("tsec");
+
+
 var countdownTimer = null;
 var count = 0;
+
+
 start.addEventListener('click', function(){
 
    
@@ -14,14 +18,14 @@ start.addEventListener('click', function(){
         countdownTimer =  setInterval(timer,1000);
     }
 
-       
-        if(count == 0){
+        if(count == 0 && (hour.value != 0 || min.value != 0 || sec.value != 0)){
             startTimer();
             start.disabled = true;
+            hour.disabled = true;
+            min.disabled = true;
+            sec.disabled = true;
             count += 1;
         }
-
-        
 
 })
 
@@ -34,9 +38,11 @@ reset.addEventListener('click', function(){
 
     if(count == 1){
         start.disabled = false;
+        hour.disabled = false;
+        min.disabled = false;
+        sec.disabled = false;
         count = 0;
     }
-
 
 })
 
@@ -46,18 +52,25 @@ if(hour.value == 0 && min .value== 0 && sec.value == 0){
         hour.value = 0;
         min.value = 0;
         sec.value = 0;
-    } else if(sec.value != 0){
+        start.disabled = false;
+        hour.disabled = false;
+        min.disabled = false;
+        sec.disabled = false;
+        count = 0;
+        clearInterval(countdownTimer);
+    } 
+    else if(sec.value != 0){
         sec.value--;
-    } else if(min.value != 0 && sec.value == 0){
+    }
+    else if(min.value != 0 && sec.value == 0){
         sec.value = 59;
         min.value--;
-    } else if(hour.value != 0 && min.value == 0){
+    } 
+    else if(hour.value != 0 && min.value == 0){
         if(sec.value == 0){
             min.value = 59;
             sec.value = 59;
         }
-
-        
         hour.value--;
     }
    
